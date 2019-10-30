850f991492b748cd5fe5613fb3856a535ea0da9f
Alpha Olomi <alphaolomi@gmail.com>
2019-10-30 23:39:02 +0300

    fix: remove password js code

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,29 +5,3 @@ jQuery('.at-banner__close').on('click', function() {
     .parent()
     .remove();
 });
-
-let passwordInput = document.getElementById('txtPassword'),
-  toggle = document.getElementById('btnToggle');
-
-function togglePassword() {
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    toggle.innerHTML = 'hide';
-  } else {
-    passwordInput.type = 'password';
-    toggle.innerHTML = 'show';
-  }
-}
-
-function checkInput() {
-  if (passwordInput.value === '') {
-    toggle.style.display = 'none';
-    toggle.innerHTML = 'show';
-    passwordInput.type = 'password';
-  } else {
-    toggle.style.display = 'block';
-  }
-}
-
-toggle.addEventListener('click', togglePassword, false);
-passwordInput.addEventListener('keyup', checkInput, false);
